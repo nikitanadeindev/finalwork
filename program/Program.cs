19d081fb6c561798d40bdcd1fc4441acf0887733
c1aymore <nikitanadeindev@gmail.com>
2022-12-21 13:28:54 +0000

    added basic array filling and printing methods

--- program/Program.cs
+++ program/Program.cs
@@ -4,3 +4,27 @@
 либо задать на старте выполнения алгоритма.
 При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 */
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-й элемент: ");
+        string value = Console.ReadLine().ToString();
+        array[i] = value;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write(array[i] + ", ");
+        else
+            Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
